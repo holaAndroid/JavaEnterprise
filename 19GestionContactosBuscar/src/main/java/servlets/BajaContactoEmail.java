ff7c63a6384fffb43f41dcb1457e038de8979013
holaAndroid <yudimeliansanchez@gmail.com>
2023-09-01 14:15:19 +0100

    GestiónContactosBuscar

---
+++ 19GestionContactosBuscar/src/main/java/servlets/BajaContactoEmail.java
@@ -0,0 +1,30 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.ContactosService;
+
+
+@WebServlet("/BajaContactoEmail")
+public class BajaContactoEmail extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		ContactosService service = new ContactosService();
+		if (!service.bajaContactoEmail(email)) {
+			request.getRequestDispatcher("baja.html")
+			.forward(request, response);
+		}else {
+			request.getRequestDispatcher("home.html")
+			.forward(request, response);
+		}
+	}
+
+}
---
+++ 19GestionContactosBuscar/src/main/java/servlets/ListarNombre.java
@@ -0,0 +1,33 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Contacto;
+import service.ContactosService;
+
+
+@WebServlet("/ListarNombre")
+public class ListarNombre extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Llamamos al metodo de ContactosService
+		ContactosService service = new ContactosService();
+		//recogo el nombre
+		String nombre= request.getParameter("nombre");
+		List<Contacto> contactos = service.listaNombre(nombre);
+		//forma de pasarse información entre los servlets setAttribute
+		request.setAttribute("contactos", contactos);
+		request.getRequestDispatcher("listado.jsp")
+		.forward(request, response);
+	}
+
+}
